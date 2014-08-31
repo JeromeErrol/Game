ad2551bce2b53b40484de42b4d6966423e8499c2
= <jeromesaltmarsh@gmail.com>
2014-08-31 19:59:39 +0200

    created new game project

---
+++ NBomUnity/Assets/src/input/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using AssemblyCSharp;
+
+public class PlayerController : MonoBehaviour
+{
+		public RocketShip rocketShip;		
+
+		void Update ()
+		{		
+				if (Input.GetMouseButtonDown (0)) {
+						MouseUtil2D.FaceMouse (rocketShip.gameObject);
+						rocketShip.FireThruster ();
+				}
+		}
+}
